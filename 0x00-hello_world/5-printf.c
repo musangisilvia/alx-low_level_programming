764473bf610212a1deb0bff46c11dc512de8bfe6
musangisilvia <musangisilvia@gmail.com>
2021-02-03 13:12:49 +0000

    using printf #1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: no arguments
+ * Description: the program starts and ends here.
+ * Return: value 0 to exit
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
