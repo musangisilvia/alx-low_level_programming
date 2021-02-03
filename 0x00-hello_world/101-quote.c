0c2f92c3164a055ee8311300ad6ff9c53e3b4317
musangisilvia <musangisilvia@gmail.com>
2021-02-03 14:47:40 +0000

    printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * @void: no arguments
+ * Description: the program starts and ends here.
+ * Return: value 0 to exit
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+return (0);
+}
