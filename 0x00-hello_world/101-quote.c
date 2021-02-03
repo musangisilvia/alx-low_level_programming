380da7dd5cc1b225c11cad0b5f1f82209e688f5c
musangisilvia <musangisilvia@gmail.com>
2021-02-03 15:42:14 +0000

    printing to stderr #4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - entry point
- * Description: the program starts and ends here.
- * Return: value 1 on success
+ * Description: the program writes to stderr.
+ * Return: value 1 on success.
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
