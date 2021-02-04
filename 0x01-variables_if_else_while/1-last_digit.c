06b9bd1ad174e63e7edabc915aecfc030133926b
musangisilvia <musangisilvia@gmail.com>
2021-02-04 13:03:17 +0000

    last digit of number #4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit < 6)
 {
-printf("Last digit of %d is %d is and less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
 else if (lastDigit == 0)
 {
