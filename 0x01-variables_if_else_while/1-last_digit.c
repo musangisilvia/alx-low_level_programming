1f4d6097cc4badb977aa884632dac9224c65e0d2
musangisilvia <musangisilvia@gmail.com>
2021-02-04 10:12:36 +0000

    random number to the variable n, check last digit #2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit > 5)
 {
-printf("Last digit of %d is %d and is  greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit < 6 && lastDigit != 0)
 {
