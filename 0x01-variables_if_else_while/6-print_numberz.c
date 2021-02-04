d1373dde84a97b868b79351a3a96087c5eb04b28
musangisilvia <musangisilvia@gmail.com>
2021-02-04 10:58:17 +0000

    print single digit numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: random number in n +ve/-ve
+ *Return: 0 on success
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar(10);
+return (0);
+}
