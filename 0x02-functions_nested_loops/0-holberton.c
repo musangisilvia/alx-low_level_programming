ee6510f0f0b7005901896fe06e8cb3b8cdaf5bdd
musangisilvia <musangisilvia@gmail.com>
2021-02-08 09:38:52 +0000

    using _putchar()

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *main - entry point.
+ *Description: program starts and ends here.
+ *Return: 0 on success.
+ */
+int main(void)
+{
+char h, o, l, b, e, r, t, n, nl;
+h = 'H';
+o = 'o';
+l = 'l';
+b = 'b';
+e = 'e';
+r = 'r';
+t = 't';
+n = 'n';
+nl = '\n';
+_putchar(h);
+_putchar(o);
+_putchar(l);
+_putchar(b);
+_putchar(e);
+_putchar(r);
+_putchar(t);
+_putchar(o);
+_putchar(n);
+_putchar(nl);
+return (0);
+}
