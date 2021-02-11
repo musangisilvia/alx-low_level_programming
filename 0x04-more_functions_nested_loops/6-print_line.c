18c46683a93c96069bc89baabec78a162722a90e
musangisilvia <musangisilvia@gmail.com>
2021-02-11 09:20:29 +0000

    place if statemment outside for loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,16 @@
 void print_line(int n)
 {
 	int i;
-
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
-	}
 	_putchar('\n');
+	}
 }
