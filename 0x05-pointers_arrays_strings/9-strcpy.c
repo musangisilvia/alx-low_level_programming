20682f8101deef29ad0c6af0a7078b148d6ebc8a
musangisilvia <musangisilvia@gmail.com>
2021-02-16 07:38:32 +0000

    add dest[j] = '\0'

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,9 +16,10 @@ char *_strcpy(char *dest, char *src)
 		lengthSrc++;
 	}
 	lengthSrc++;
-	for (j = 0; j <= lengthSrc; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[j] = src[j];
 	}
+	dest[j] = '\0';
 	return (dest);
 }
