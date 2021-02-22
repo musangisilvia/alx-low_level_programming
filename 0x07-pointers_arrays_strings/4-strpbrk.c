b09e820ec6eb88aa51169935ad33a4b29c60ba02
musangisilvia <musangisiliva@gmail.com>
2021-02-22 20:22:41 +0000

    initializes p to 0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
   *_strpbrk - seraches a string for any of a set of bytes.
-  *it locates the first occurence in the string s of any of 
+  *it locates the first occurence in the string s of any of
   *the bytes in the string accept.
   *@s: pointer to string.
   *@accept: pointer to strin to be matched.
@@ -15,13 +15,18 @@ char *_strpbrk(char *s, char *accept)
 	char *p;
 	int counter, counter2;
 
+	p = 0;
+
 	for (counter = 0; s[counter] != '\0'; counter++)
+		p = &needle[i];
 	{
 		for (counter2 = 0; accept[counter2]; counter2++)
 		{
 			if (accept[counter2] == s[counter])
+			{
 				p = &s[counter];
-			break;
+				return (p);
+			}
 		}
 	}
 	return (p);
