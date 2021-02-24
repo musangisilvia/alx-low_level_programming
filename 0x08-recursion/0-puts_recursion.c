f316950a863cf0909738c6ff6a924af1cb8967f9
musangisilvia <musangisiliva@gmail.com>
2021-02-24 15:16:20 +0000

    remove misplaced semicolon

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* base case */;
+	if (*s == '\0') /* base case */
+	{
+		_putchar('\n');
 		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-
-	_putchar('\n');
 }
