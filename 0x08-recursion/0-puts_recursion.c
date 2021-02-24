be9794eb46bb8715c7ce59ee0bfecb5652686359
musangisilvia <musangisiliva@gmail.com>
2021-02-24 15:12:34 +0000

    add curly braces for if stmt

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,9 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0') /* base case */;
-	{
-		_putchar('\n');
 		return;
-	}
 	_putchar(*s);
-	_puts_recursion(++s);
+	_puts_recursion(s + 1);
+
+	_putchar('\n');
 }
