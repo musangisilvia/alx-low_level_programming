bcf81196a17c93ee28d6cdcd348bc44e31358364
musangisilvia <musangisiliva@gmail.com>
2021-02-26 11:53:42 +0000

    add a program that prints number of arguments passed to it followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  *main - prints the number of arguments passed to it.
+  *@argc: number of arguemnts.
+  *@argv: array of size argc.
+  *
+  *Return: 0 on success.
+  */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
