48b1373afb2d99cb28ddc33fa4f239b78586e8d9
musangisilvia <musangisiliva@gmail.com>
2021-03-02 08:52:33 +0000

    add a function similar to strdup in c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  *_strdup - retrurns a pointer to a newly alloctaed
+  *space in memory which contains a copy of the string
+  *passed.
+  *@str: pointer to string being duplicated.
+  *
+  *Return: NULL if str is NULL.
+  *pointer to duplicated string on success.
+  *NULL if memory was insufficient.
+  */
+char *_strdup(char *str)
+{
+	char *nstr, *p;
+	int len, i;
+
+	/* check is str is null */
+	if (!*str)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc(len + 1);
+	p = nstr;
+
+	for (i = 0; i < len; i++)
+	{
+		*p++ = *str++;
+	}
+	*p = '\0';
+
+	return (nstr);
+}
