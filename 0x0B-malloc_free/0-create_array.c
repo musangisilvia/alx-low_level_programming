6835b278258710b95f62fae0d23bbcb548939046
musangisilvia <musangisiliva@gmail.com>
2021-03-02 09:22:16 +0000

    if to check if malloc was successful

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,17 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (size <= 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
 
 	arr = malloc(sizeof(char) * size);
+
+	/* check if malloc was successful */
+
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
