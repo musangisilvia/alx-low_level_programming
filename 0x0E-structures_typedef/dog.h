165fb9e8bc3e176db9745eb2782dc0b335732ef5
musangisilvia <musangisilvia@gmail.com>
2021-03-08 12:18:08 +0000

    change position of first brace in struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
   *struct dog - creates structure of a dog.
   *@name: first member.
   *@owner: second member.
   *@age: third member.
   */
-struct dog {
+struct dog
+{
 	char *name; 
-	char *owner;
 	float age;
+	char *owner;
 };
 typedef struct dog dog_t;
 
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H*/
