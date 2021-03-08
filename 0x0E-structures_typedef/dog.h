b9f23802edeca8580e62fc655230d69a682305e0
musangisilvia <musangisilvia@gmail.com>
2021-03-08 18:30:57 +0000

    add struct definition in header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,19 @@
 #ifndef STRTYP
+
 #define STRTYP
 
 /**
-  *struct dog - creates structure of a dog.
-  *@name: first member.
-  *@age: second member.
-  *@owner: third member.
-  *Description: the structure of a dog.
+  *struct dog - structure of a dog.
+  *@name: name of dog.
+  *@age: age of dog.
+  *@owner: owner of dog.
+  *Description: structure of dog, three members.
   */
 struct dog
 {
-	char *name; 
-	float age;
+	char *name;
+	int age;
 	char *owner;
 };
-typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /* STRTYP */
+#endif /*STRTYP*/
