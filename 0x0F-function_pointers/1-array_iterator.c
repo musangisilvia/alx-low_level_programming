16899b442da23b62eb1574d3f56857ff3a3947fe
musangisilvia <musangisilvia@gmail.com>
2021-03-10 09:34:11 +0000

    check if array exists and size is not 0

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
-	for (i = 0; i < size; i++)
+	if (array && size > 0 && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
