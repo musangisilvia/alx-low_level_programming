2371c8a0b1a8b805a0199920fd4cdd3d92ab3dab
musangisilvia <musangisilvia@gmail.com>
2021-03-10 09:39:23 +0000

    add if to check if array exists and size is > 0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,21 +11,23 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && size > 0 && cmp)
 	{
-		cmp(array[i]);
-		if (cmp(array[i]) > 0)
+		for (i = 0; i < size; i++)
 		{
-			break;
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
+			{
+				break;
+			}
+			if ((cmp(array[i]) == (-1)))
+			{
+				i = -1;
+				break;
+			}
 		}
-		if ((cmp(array[i]) == (-1)))
-		{
-			i = -1;
-			break;
-		}
-	}
 	return (i);
+	}
 }
