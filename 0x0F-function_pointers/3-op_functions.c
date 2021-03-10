9482dd5c4e6b0349bfd980c8c749ee9da52640b8
musangisilvia <musangisilvia@gmail.com>
2021-03-10 13:59:04 +0000

    add if statement to check if b is 0

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
   *op_add - return the sum of 2 numbers.
   *@a: integer.
@@ -7,7 +8,7 @@
   */
 int op_add(int a, int b)
 {
-	return ((a) + (b));
+	return (a + b);
 }
 
 /**
@@ -19,7 +20,7 @@ int op_add(int a, int b)
   */
 int op_sub(int a, int b)
 {
-	return ((a) - (b));
+	return (a - b);
 }
 
 /**
@@ -31,7 +32,7 @@ int op_sub(int a, int b)
   */
 int op_mul(int a, int b)
 {
-	return ((a) * (b));
+	return (a * b);
 }
 
 /**
@@ -43,7 +44,12 @@ int op_mul(int a, int b)
   */
 int op_div(int a, int b)
 {
-	return ((a) / (b));
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 /**
@@ -55,5 +61,10 @@ int op_div(int a, int b)
   */
 int op_mod(int a, int b)
 {
-	return ((a) % (b));
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
