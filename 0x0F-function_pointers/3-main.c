7a21e900d422d2e10b99a6a0d460d8273358fcbf
musangisilvia <musangisilvia@gmail.com>
2021-03-10 13:39:10 +0000

    add main function to get and check arguments passed

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - check arguments
+  *@argc: argument count
+  *@argv: argument vector.
+  *
+  *Return: error if number of arguments is wrong
+  */
+int main(int argc, char *argv[])
+{
+	int a = 0, b = 0, res = 0;
+	char s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
+	return (0);
+}
+
