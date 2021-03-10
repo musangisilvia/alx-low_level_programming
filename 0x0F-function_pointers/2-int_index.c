c1880b95f87dd29f7f527c650c081ab0a3630865
musangisilvia <musangisilvia@gmail.com>
2021-03-10 09:52:25 +0000

    initialize i to -1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
+	i = -1;
 	if (array && cmp)
 	{
 
 		if (size <= 0)
-			return (-1);
+			return (i);
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
