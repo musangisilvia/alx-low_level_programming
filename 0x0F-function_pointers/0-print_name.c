393ece925d43a2d89a8b68672d07650331cea83a
musangisilvia <musangisilvia@gmail.com>
2021-03-10 09:30:53 +0000

    check if name and pointer exist/ not null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,3 @@
-#include <unistd.h>
-/**
-  *_putchar - write to stdout.
-  *@c: pointer to text.
-  *
-  *Return: void.
-  */
-void _putchar(char *c)
-{
-	write(1, c, 1);
-}
-
 /**
   *print_name - prints a name.
   *@name: pointer to name.
@@ -19,6 +7,7 @@ void _putchar(char *c)
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name && f)
+		f(name);
 }
 
