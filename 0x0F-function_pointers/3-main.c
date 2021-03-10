8044a6c89c2ad38b0c24d9b74db85423085534f6
musangisilvia <musangisilvia@gmail.com>
2021-03-10 14:14:42 +0000

    add if to chekc if theres onlu one operator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	/* check if theres only one operator*/
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	s = argv[2][0];
 	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
