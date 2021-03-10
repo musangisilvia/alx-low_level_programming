c86e9161e39a4b1b056f3d3c526a7403c94ec2f8
musangisilvia <musangisilvia@gmail.com>
2021-03-10 09:44:52 +0000

    return i

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,10 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
 	if (array && cmp)
 	{
+
+		if (size <= 0)
+			return (-1);
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
