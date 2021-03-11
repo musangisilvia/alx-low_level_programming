8189919e1e464161a98beebec4834ac6e4e4413b
musangisilvia <musangisilvia@gmail.com>
2021-03-11 11:06:47 +0000

    check if separator is null, treat as empty string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (n == 0)
 		return;
-
+	if (separator == NULL)
+		separator = "";
 	va_start(strings, n);
 
 	for (i = 0; i < n; i++)
@@ -29,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			return;
 		}
 		printf("%s", s);
-		if (i < n - 1 && separator != NULL)
+		if (i < n - 1)
 		{
 			printf("%s", separator);
 		}
