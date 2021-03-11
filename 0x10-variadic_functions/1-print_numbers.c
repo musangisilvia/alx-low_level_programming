7c0c790025bbb9bf3b70e5e16ea7f122770f6dee
musangisilvia <musangisilvia@gmail.com>
2021-03-11 15:49:45 +0000

    remove if to check if n == 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list numbers;
 
-	if (n == 0)
-		return;
-	if (separator == NULL)
-		separator = "";
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
