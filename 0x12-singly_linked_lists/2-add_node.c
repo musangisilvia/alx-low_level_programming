fadab4ae2db306d305ce9d7bbf740d2ed8ab0cdd
musangisilvia <musangisilvia@gmail.com>
2021-03-18 10:11:36 +0000

    add function that finds lengt of a string

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 /**
+  *_strlen - finds length of a string.
+  *@str: string.
+  *
+  *Return: lengt of string.
+  */
+int _strlen(const char *str)
+{
+	int i;
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
   *add_node - adds a new node at the beginning of a list_t.
   *@head: pointer to first element of the list.
   *@str: string to be duplicated.
@@ -16,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	/* temp now points to first element*/
 	temp->next = *head;
 	temp->str = strdup(str);
-	temp->len = strlen(str);
+	temp->len = _strlen(str);
 
 	*head = temp;
 
