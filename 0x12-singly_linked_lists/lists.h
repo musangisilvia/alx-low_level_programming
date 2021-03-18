c428f4ddce5669af26cde02f871c99460fbc2597
musangisilvia <musangisilvia@gmail.com>
2021-03-18 10:05:55 +0000

    add a function prototype of a functionwq that adds elements at the begining of a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,5 +22,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /*LISTS_H*/
