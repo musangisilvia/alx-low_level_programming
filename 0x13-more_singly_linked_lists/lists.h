c05afe29548b19e3c73cc11048d5f41835283cd1
musangisilvia <musangisilvia@gmail.com>
2021-03-22 08:21:28 +0000

    add a function prototype for a function that prints all elemnts of a list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * struct listint_s - singly linked list
+  * @n: integer
+  * @next: points to the next node
+  *
+  * Description: singly linked list node structure
+  * for Holberton project
+  */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+/* Function protoypes */
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
