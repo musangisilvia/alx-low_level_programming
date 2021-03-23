eb8c32dce37b2ff9bb746fe366f2d4ff4d34d6b3
musangisilvia <musangisilvia@gmail.com>
2021-03-23 06:13:50 +0000

    add an if to check if double pointer is null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
 }
