689385b618c8a265bb16681c2803f763ab68f5f7
musangisilvia <musangisilvia@gmail.com>
2021-03-23 07:21:19 +0000

    add condition to check if count is == to index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = new_node;
 			return (new_node);
 		}
-		else
+		if (count + 1 == idx)
 		{
 			new_node->next = temp->next;
 			temp->next = new_node;
