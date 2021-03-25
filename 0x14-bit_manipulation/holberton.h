04bd99869b5974a8ccbe6f0b0c239090e5d292eb
musangisilvia <musangisilvia@gmail.com>
2021-03-25 11:11:45 +0000

    add a function that sets a bit to 0 at index.

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -11,5 +11,6 @@ unsigned int _strlen(const char *s);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* HOLBERTON_H */
