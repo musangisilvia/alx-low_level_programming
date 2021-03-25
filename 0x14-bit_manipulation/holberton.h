5e951091388d450a3586d6e988327dcb7b0f66c2
musangisilvia <musangisilvia@gmail.com>
2021-03-25 08:44:12 +0000

    add function prototype for 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/* C standard library files */
+#include <stdio.h>
+
+/* Function Prototypes */
+unsigned int binary_to_uint(const char *b);
+unsigned int _strlen(const char *s);
+unsigned int getp(int x, int y);
+
+#endif /* HOLBERTON_H */
