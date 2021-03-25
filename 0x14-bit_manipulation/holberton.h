0f0cafece1c6ab00e0001ab8bf91cfca5029781e
musangisilvia <musangisilvia@gmail.com>
2021-03-25 09:40:54 +0000

    addfunction prototype for 1-print_binary.c

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,11 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-/* C standard library files */
-#include <stdio.h>
 
 /* Function Prototypes */
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int _strlen(const char *s);
 unsigned int getp(int x, int y);
+void print_binary(unsigned long int n);
 
 #endif /* HOLBERTON_H */
