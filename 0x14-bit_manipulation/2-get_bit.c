dbe12f48e079f5384f6fc9f8176a4b76c2bb0386
musangisilvia <musangisilvia@gmail.com>
2021-03-25 11:22:45 +0000

    check if index is larger than 64

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bitstatus;
 
-	bitstatus = (n >> index) & 1;
-	if (!(n >> index))
+	if (index > 64)
 		return (-1);
+	bitstatus = (n >> index) & 1;
+
 	return (bitstatus);
 }
