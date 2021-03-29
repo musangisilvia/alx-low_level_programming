5eb81a1e1bb4e005948c3b3a08379e3aeee7ce90
musangisilvia <musangisilvia@gmail.com>
2021-03-29 16:26:36 +0000

    change ( == -1) to ( < 0)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_WRONLY | O_EXCL | O_APPEND);
-	if (file == -1)
+	if (file < 0)
 		return (-1);
 	i = 0;
 	while (text_content[i] != '\0')
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		app = write(file, text_content, i);
-		if (app == -1)
+		if (app < 0)
 			return (-1);
 	}
 
