07ab8c3cd4f0066f885b21e056324c68649d8887
musangisilvia <musangisilvia@gmail.com>
2021-03-29 11:29:26 +0000

    add a return stmt to else

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i;
+	int file, i, wr;
 
 	if (filename == NULL)
 		return (-1);
@@ -26,9 +26,10 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		write(file, text_content, i);
+		wr = write(file, text_content, i);
+		if (wr == -1)
+			return (-1);
+		return (1);
 	}
-
 	close(file);
-	return (1);
 }
