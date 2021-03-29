4af9ba4ca47e1ca78ee5d24f1da53b99859104f8
musangisilvia <musangisilvia@gmail.com>
2021-03-29 16:17:28 +0000

    add O_EXCL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_APPEND);
+	file = open(filename, O_RDWR | O_EXCL | O_APPEND);
 	if (file == -1)
 		return (-1);
 	i = 0;
@@ -23,12 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		app = write(file, text_content, i);
 		if (app == -1)
 			return (-1);
-		return (1);
 	}
-	else
-	{
-		close(file);
-		return (1);
-	}
+
 	close(file);
+	return (1);
 }
