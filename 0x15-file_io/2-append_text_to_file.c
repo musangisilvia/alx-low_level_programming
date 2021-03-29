4c0d76b5e3ee55ce70099363b0c647a277140024
musangisilvia <musangisilvia@gmail.com>
2021-03-29 13:38:11 +0000

    add a function that appends texts to existing files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+  * append_text_to_file - appends text at the end of a file.
+  * @filename: pointer to nam eof file.
+  * @text_content: null terminated string.
+  *
+  * Return: 1 on success, -1 on failure.
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, app, i;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
+		return (-1);
+	i = 0;
+	while (text_content[i] != '\0')
+		i++;
+	if (text_content != NULL)
+	{
+		app = write(file, text_content, i);
+		if (app == -1)
+			return (-1);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
+	close(file);
+}
