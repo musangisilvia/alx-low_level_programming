d7084f652d23ea9d1d0f5225f7dfe5002826d18a
musangisilvia <musangisilvia@gmail.com>
2021-03-29 16:10:48 +0000

    remove ele

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,15 +24,14 @@ int create_file(const char *filename, char *text_content)
 		close(file);
 		return (1);
 	}
-	else
+
+	wr = write(file, text_content, i);
+	if (wr == -1)
 	{
-		wr = write(file, text_content, i);
-		if (wr == -1)
-		{
-			close(file);
-			return (-1);
-		}
+		close(file);
+		return (-1);
 	}
+
 	close(file);
 	return (1);
 }
