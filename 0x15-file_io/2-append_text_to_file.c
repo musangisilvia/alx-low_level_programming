6d75c51f4e66a165e96dc570d67ccb1e59c58b42
musangisilvia <musangisilvia@gmail.com>
2021-03-29 16:23:52 +0000

    change RDWR to WRONLY

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_EXCL | O_APPEND);
+	file = open(filename, O_WRONLY | O_EXCL | O_APPEND);
 	if (file == -1)
 		return (-1);
 	i = 0;
