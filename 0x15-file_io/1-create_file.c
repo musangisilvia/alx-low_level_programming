1657b870eab05394861fa6c64d49b28abdd6d4eb
musangisilvia <musangisilvia@gmail.com>
2021-03-29 15:55:37 +0000

    change return value if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,14 +22,17 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(file);
-		return (-1);
+		return (1);
 	}
 	else
 	{
 		wr = write(file, text_content, i);
 		if (wr == -1)
+		{
 			return (-1);
+		}
+		close(file);
 		return (1);
 	}
-	close(file);
+
 }
