01709fa635d3028151cb2578a42f43e8278f5c54
musangisilvia <musangisilvia@gmail.com>
2021-03-29 16:29:10 +0000

    close file if wr is < 0

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		app = write(file, text_content, i);
 		if (app < 0)
+		{
+			close(file);
 			return (-1);
+		}
 	}
 
 	close(file);
