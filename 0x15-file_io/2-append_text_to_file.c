5ca245b5afa1d35c643c9e2eb9640bec396b60eb
musangisilvia <musangisilvia@gmail.com>
2021-03-30 09:31:21 +0300

    Remove trailing whitespace

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	file = open(filename, O_WRONLY | O_EXCL | O_APPEND);
 	if (file < 0)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		i = 0;
