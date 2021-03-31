978b937de3d59be8dcaa6571e32f793edb5785b3
musangisilvia <musangisilvia@gmail.com>
2021-03-31 10:28:09 +0000

    fix the segmentation error when text content is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,24 +12,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	/*finde length of text content*/
-	i = 0;
-	while (text_content[i])
-		i++;
-	if (text_content == NULL)
-	{
-		close(file);
-		return (1);
-	}
 
-	wr = write(file, text_content, i);
-	if (wr == -1)
+	if (text_content)
 	{
-		close(file);
-		return (-1);
+		i = 0;
+		while (text_content[i])
+			i++;
+		wr = write(file, text_content, i);
+		if (wr != i)
+			return (-1);
 	}
 
 	close(file);
