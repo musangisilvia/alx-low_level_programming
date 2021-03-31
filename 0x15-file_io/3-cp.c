4dbc8bf36a253a69be16eeccd312cc41c8f3af00
musangisilvia <musangisilvia@gmail.com>
2021-03-31 09:23:28 +0000

    correct betty errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,18 +13,18 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-	if ((file1 = open(argv[1], O_RDONLY, 0)) == -1)
+	file1 =  open(argv[1], O_RDONLY, 0);
+	if (file1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if ((file2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664)) == -1)
+	file2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (file2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	while ((size = read(file1, buf, 1024)) > 0)
 	{
