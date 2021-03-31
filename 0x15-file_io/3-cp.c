36914dbddbe7fa0541594c863ed4d7622603e161
musangisilvia <musangisilvia@gmail.com>
2021-03-31 09:35:48 +0000

    check if size is -1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,21 +30,20 @@ int main(int argc, char *argv[])
 	{
 		if (write(file2, buf, size) != size)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
-	close(file1);
-	close(file2);
+	if (size == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(100);
+	}
 	if (close(file1) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1), exit(100);
 	}
 	if (close(file2) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2), exit(100);
 	}
 	return (0);
-}
