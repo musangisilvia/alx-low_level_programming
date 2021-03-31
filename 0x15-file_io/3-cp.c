8be8a469a1da229b319bb809abb30691010bbbc4
musangisilvia <musangisilvia@gmail.com>
2021-03-31 09:10:40 +0000

    add a program that copies one file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+  * main - copy one file to another.
+  * @argc: argument count.
+  * @argv: argument vector.
+  *
+  * Return: 0.
+  */
+int main(int argc, char *argv[])
+{
+	int file1, file2, size;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if ((file1 = open(argv[1], O_RDONLY, 0)) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if ((file2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664)) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((size = read(file1, buf, 1024)) > 0)
+	{
+		if (write(file2, buf, size) != size)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close(file1);
+	close(file2);
+	if (!(close(file1)))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1);
+		exit(100);
+	}
+	if (!(close(file2)))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2);
+		exit(100);
+	}
+	return (0);
+}
