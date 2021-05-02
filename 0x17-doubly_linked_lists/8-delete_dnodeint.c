0f008f27fe9225de23cccfd50c217c1adc527c61
musangisilvia <musangisilvia@gmail.com>
2021-05-02 16:18:41 +0000

    correct return value

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,9 +10,10 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp, *temp2, *tp;
+	unsigned int len = dlistint_len2(*head);
 
-	if (*head == NULL)
-		return (0);
+	if (*head == NULL || len < index)
+		return (-1);
 	temp = *head;
 	if (index == 0 && (*head)->next == NULL)
 	{
@@ -37,14 +38,31 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			temp2->next = NULL, free(tp);
 			return (1);
 		}
-		else
-		{
-			temp2 = temp->prev;
-			temp2->next = temp->next;
-			temp->next->prev = temp2;
-		}
-		return (1);
+		temp2 = temp->prev;
+		temp2->next = temp->next;
+		temp->next->prev = temp2;
 	}
 	free(temp);
 	return (1);
+}
+
+/**
+  * dlistint_len2 - returns number of elements in a lineked dlistint_t list.
+  * @h: pointer to list.
+  *
+  * Return: number of elements in list.
+  */
+unsigned int dlistint_len2(dlistint_t *h)
+{
+	dlistint_t *temp;
+	unsigned int count;
+
+	temp = h;
+	count = 0;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
 }
