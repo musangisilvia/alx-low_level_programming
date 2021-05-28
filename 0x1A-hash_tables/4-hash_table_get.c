9a1bd97b759e69c36612ebe08128b0cffa4aa95f
musangisilvia <musangisilvia@gmail.com>
2021-05-28 07:06:14 +0000

    check for when ht is null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,12 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	temp = ht->array[index];
-
-	if (ht == NULL || key == NULL)
-		return (NULL);
 
 	while (temp != NULL)
 	{
