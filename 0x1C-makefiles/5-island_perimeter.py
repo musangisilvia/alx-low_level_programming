0be90a4dc952f8796244f8ad31150463937d9bde
musangisilvia <musangisilvia@gmail.com>
2021-07-02 20:33:11 +0000

    add if statements

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,8 +20,10 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] is lz:
-                index_list.append(j)
-
-    perimeter = 1 * ((len(index_list) + 1) + (len(index_list) - 1)) + 2
+                perimeter += 4
+                if i > 0 and grid[i][j - 1]:
+                    perimeter -= 2
+                if j > 0 and grid[i - 1][j]:
+                    perimeter -= 2
 
     return perimeter
