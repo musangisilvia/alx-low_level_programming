5525736078f7a907f33fac635ffb71b786919fe7
musangisilvia <musangisilvia@gmail.com>
2021-09-01 10:38:13 +0000

    add task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,23 +9,34 @@
   */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t idx = 0;
+	size_t i, last_idx, idx = 0;
 	int m;
 
 	if (array == NULL)
 		return (-1);
 
-	while (idx <= size - 1)
+	last_idx = size - 1;
+
+	while (idx <= last_idx)
 	{
-		m = (idx + (size - idx)) / 2;
-		if (array[m] == value)
-			return (m);
+		printf("Searching in array: ");
+
+		for (i = idx; i <= last_idx; i++)
+		{
+			printf("%d", array[i]);
+			if (i != last_idx)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		m = (idx + last_idx) / 2;
+
 		if (array[m] < value)
 			idx = m + 1;
+		else if (array[m] > value)
+			last_idx = m - 1;
 		else
-			size = m - 1;
-		idx++;
+			return (m);
 	}
-
 	return (-1);
 }
